e9e6fb08fafeb32d6309dfcb148341cd3c02be64
rob cheung <kousun12@gmail.com>
2019-10-08 18:30:07 -0500

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-function help {
-	echo "Usage: $(basename "$0") [<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease]"
-}
-
 if [ "$1" = "help" ]; then
-	help
+	echo "Usage: $(basename "$0") [major | minor | patch | premajor | preminor | prepatch | prerelease]"
 	exit
 fi
 
@@ -14,9 +10,9 @@ release=$1
 if [ -d ".git" ]; then
 	changes=$(git status --porcelain)
 	if [ -z "${changes}" ]; then
-	  version=${$release:-minor}
-    echo "$version"
-#    yarn deploy
+	  version=${release:-minor}
+    yarn version --"$version"
+    yarn deploy
 	else
 		echo "Git status dirty"
 		exit 1
