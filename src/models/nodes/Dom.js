4a5086237f0768bddcba1dbad95ff573d2bf752e
rob cheung <kousun12@gmail.com>
2019-10-09 20:18:37 -0500

    fix stephen

--- src/models/nodes/Dom.js
+++ src/models/nodes/Dom.js
@@ -19,10 +19,25 @@ export class DomNode extends NodeBase<{}, { html: string }, null> {
     state: {},
   };
   domNode: HTMLElement;
+  inserted: boolean = false;
 
   onAddToGraph = () => {
     this.domNode = document.createElement('div');
-    document.getElementsByTagName('body')[0].appendChild(this.domNode);
+    this._tryInsert();
+  };
+
+  _tryInsert = () => {
+    if (this.inserted) {
+      return;
+    }
+    const root = document.getElementById('graph-root');
+    const scalable = document.getElementById('graph-scalable');
+    if (root && scalable) {
+      root.insertBefore(this.domNode, scalable);
+      this.inserted = true;
+    } else {
+      setTimeout(() => this._tryInsert(), 20);
+    }
   };
 
   willReceiveProps = (newProps: Object, prevProps: Object) => {
