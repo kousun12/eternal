e64bb1add85600edda25e48bb6870da58885c96e
rob cheung <kousun12@gmail.com>
2019-10-10 18:41:27 -0500

    add an example gpgpu

--- src/components/ExampleSearch.js
+++ src/components/ExampleSearch.js
@@ -15,6 +15,7 @@ export const examples = [
   require('models/examples/the music while the music lasts.json'),
   require('models/examples/stephen wolfram.json'),
   require('models/examples/shaders.json'),
+  require('models/examples/gpgpu.json'),
   require('models/examples/percept nets.json'),
   require('models/examples/soundfont-midi.json'),
   require('models/examples/soundfont-tone.json'),
