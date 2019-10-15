f80cf024f998cb91bddf18364ab3debf2dc711d6
rob cheung <kousun12@gmail.com>
2019-10-15 00:54:35 -0500

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,13 @@ fi
 
 release=$1
 
-if [ -d ".git" ]; then
-  changes=$(git status --porcelain)
-  if [ -z "${changes}" ]; then
-    version=${release:-minor}
-    yarn version --"$version"
-    yarn deploy
-    git push --tags
-  else
-    echo "Git status dirty"
-    exit 1
-  fi
+changes=$(git status --porcelain)
+if [ -z "${changes}" ]; then
+  version=${release:-minor}
+  yarn version --"$version"
+  yarn deploy
+  git push --tags
 else
-  echo "No git"
+  echo "git dirty, commit first"
   exit 1
 fi
